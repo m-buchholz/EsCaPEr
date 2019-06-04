a682afa6f17810808be504069dc1e1d962792b10
Maximilian Buchholz <maximilianbuchholz@web.de>
2019-06-05 01:24:55 +0200

    link scenes with buttons

---
+++ Assets/TheGame/Scripts/ChangeSceneOnButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneOnButton : MonoBehaviour
+{
+
+    public Button button;
+    public int scene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(ChangeScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ChangeScene()
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
