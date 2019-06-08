1da8cd4a762ab06dc95e1cf06559ccfba4f73761
Maximilian Buchholz <maximilianbuchholz@web.de>
2019-06-08 03:31:13 +0200

    blink animation added

---
+++ Assets/TheGame/Scripts/Blink.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blink : MonoBehaviour
+{
+
+    public Button button;
+    private bool isover=false;
+
+    private void OnMouseOver()
+    {
+        isover = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
