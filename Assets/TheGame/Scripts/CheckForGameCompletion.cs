24c88061a085762ee05fd0ac48c98b7718c09544
RicoAdler <33716094+RicoAdler@users.noreply.github.com>
2019-06-09 22:57:52 +0200

    another changes

--- Assets/TheGame/Scripts/CheckForGameCompletion.cs
+++ Assets/TheGame/Scripts/CheckForGameCompletion.cs
@@ -7,8 +7,7 @@ using UnityEngine.UI;
 
 public class CheckForGameCompletion : MonoBehaviour
 {
-    public Sprite image1;
-    public Sprite image2;
+
     public GameObject utensil1, utensil2, utensil3, utensil4, utensil5;
     public void CheckForCompletion()
     {
@@ -22,29 +21,4 @@ public class CheckForGameCompletion : MonoBehaviour
         }
     }
 
-    public void ChangeImage()
-    {
-
-
-        if(utensil1.GetComponent<CollisionControllerForAssignment>().isCorrect() == false ||
-            utensil2.GetComponent<CollisionControllerForAssignment>().isCorrect() == false ||
-            utensil3.GetComponent<CollisionControllerForAssignment>().isCorrect() == false ||
-            utensil4.GetComponent<CollisionControllerForAssignment>().isCorrect() == false ||
-            utensil5.GetComponent<CollisionControllerForAssignment>().isCorrect() == false)
-        {
-            GetComponent<Image>().sprite = image1;
-            StartCoroutine(Timer());
-            GetComponent<Image>().sprite = image2;
-
-        }
-        
-    }
-
-
-    IEnumerator Timer()
-    {
-        print(Time.time);
-        yield return new WaitForSeconds(5000);
-        print(Time.time);
-    }
 }
