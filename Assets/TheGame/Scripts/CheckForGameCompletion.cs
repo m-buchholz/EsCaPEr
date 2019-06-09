54596c6caeef6cd9d0dfe5e3f66d9abd309dc1e7
RicoAdler <33716094+RicoAdler@users.noreply.github.com>
2019-06-09 22:55:11 +0200

    change order on game one

--- Assets/TheGame/Scripts/CheckForGameCompletion.cs
+++ Assets/TheGame/Scripts/CheckForGameCompletion.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class CheckForGameCompletion : MonoBehaviour
 {
+    public Sprite image1;
+    public Sprite image2;
     public GameObject utensil1, utensil2, utensil3, utensil4, utensil5;
     public void CheckForCompletion()
     {
@@ -17,5 +20,31 @@ public class CheckForGameCompletion : MonoBehaviour
         {
             SceneManager.LoadScene("Room1", LoadSceneMode.Single);
         }
+    }
+
+    public void ChangeImage()
+    {
+
+
+        if(utensil1.GetComponent<CollisionControllerForAssignment>().isCorrect() == false ||
+            utensil2.GetComponent<CollisionControllerForAssignment>().isCorrect() == false ||
+            utensil3.GetComponent<CollisionControllerForAssignment>().isCorrect() == false ||
+            utensil4.GetComponent<CollisionControllerForAssignment>().isCorrect() == false ||
+            utensil5.GetComponent<CollisionControllerForAssignment>().isCorrect() == false)
+        {
+            GetComponent<Image>().sprite = image1;
+            StartCoroutine(Timer());
+            GetComponent<Image>().sprite = image2;
+
+        }
+        
+    }
+
+
+    IEnumerator Timer()
+    {
+        print(Time.time);
+        yield return new WaitForSeconds(5000);
+        print(Time.time);
     }
 }
