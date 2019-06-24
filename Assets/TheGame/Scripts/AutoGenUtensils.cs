11ac2c37f8d670bea7af569b4044a13038fdae3c
RicoAdler <33716094+RicoAdler@users.noreply.github.com>
2019-06-24 21:20:36 +0200

    generate resources folder

---
+++ Assets/TheGame/Scripts/AutoGenUtensils.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoGenUtensils : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateObj(0, 0, "lol");
+    }
+
+    private void GenerateObj(float x, float y, string utensil)
+    {
+        GameObject gameObject = (GameObject)Instantiate(Resources.Load("Assets/TheGame/Prefabs/Messzylinder"));
+
+    }
+
+
+}
