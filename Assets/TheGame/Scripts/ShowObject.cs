3b56f3bfeb3450b94fbcccd53c988123986c38eb
Maximilian Buchholz <maximilianbuchholz@web.de>
2019-06-30 21:48:07 +0200

    add support poster for danger symbols and change room2 for minigame

--- Assets/TheGame/Scripts/ShowObject.cs
+++ Assets/TheGame/Scripts/ShowObject.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Show an image as overlay
+/// </summary>
+
 public class ShowObject : MonoBehaviour
 {
     public GameObject showObject;
+    public Sprite sourceImage;
     private Button button;
 
     void Start()
@@ -16,6 +21,7 @@ public class ShowObject : MonoBehaviour
 
     private void show()
     {
+        showObject.GetComponentInChildren<Image>().sprite = sourceImage;
         showObject.SetActive(true);
     }
 }
