14d52b6c9936aa91f2ba6a526eb9b5ac72bd1c3a
Maximilian Buchholz <maximilianbuchholz@web.de>
2019-07-04 19:42:02 +0200

    add some nice music

---
+++ Assets/TheGame/Scripts/NextAudioTitle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NextAudioTitle : MonoBehaviour
+{
+    private Button button;
+    public AudioClip audio1, audio2, audio3;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(NextTitle);
+    }
+
+    private void NextTitle()
+    {
+        if (GameObject.Find("Background Audio"))
+        {
+            AudioSource bAudio = GameObject.Find("Background Audio").GetComponent<AudioSource>();
+            if (bAudio.clip == audio1) bAudio.clip = audio2;
+            else if (bAudio.clip == audio2) bAudio.clip = audio3;
+            else if (bAudio.clip == audio3) bAudio.clip = audio1;
+            bAudio.Play();
+        }
+    }
+}
