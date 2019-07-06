d24f73d80ca8f81d2e3e2a9cac81f47f42bb5c9d
EbenjaKRAL <33632743+EbenjaKRAL@users.noreply.github.com>
2019-07-06 18:09:39 +0200

    only one key at once

--- Assets/TheGame/Scripts/Raum1_Minispiel2/CheckMetal.cs
+++ Assets/TheGame/Scripts/Raum1_Minispiel2/CheckMetal.cs
@@ -23,6 +23,8 @@ public class CheckMetal : MonoBehaviour
     public GameObject aluminiumobj;
     public GameObject nickelobj;
     
+    public GameObject key;
+    
     
      void Start ()
      {
@@ -34,29 +36,28 @@ public class CheckMetal : MonoBehaviour
 void OnTriggerEnter2D (Collider2D collision)
     {
         
-         if (collision.transform == titan)
+         if (collision.transform == titan && key.activeSelf == false)
          {
            metalinoven = true;
            metaltype = 1;
-           
          }
          
-          if (collision.transform == eisen)
+          if (collision.transform == eisen && key.activeSelf == false)
          {
            metalinoven = true;
            metaltype = 2;
          }
-          if (collision.transform == aluminium)
+          if (collision.transform == aluminium && key.activeSelf == false)
          {
            metalinoven = true;
            metaltype = 3;
          }
-          if (collision.transform == nickel)
+          if (collision.transform == nickel && key.activeSelf == false)
          {
            metalinoven = true;
            metaltype = 4;
          }
-          if (collision.transform == platin)
+          if (collision.transform == platin && key.activeSelf == false)
          {
            metalinoven = true;
            metaltype = 5;
